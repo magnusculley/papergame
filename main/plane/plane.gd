496d368b4d9822a4e79c24a8800500b1234246f6
magnusculley <magnus@udel.edu>
2025-04-10 15:22:37 -0400

    physics maybe working

--- main/plane/plane.gd
+++ main/plane/plane.gd
@@ -1,26 +1,30 @@
 extends CharacterBody2D
-var speed = 2
-var direction=Vector2(100,0)
-const ANGLE =.01
-const GRAVITY = Vector2(0,2000)
-const DRAG=.002
-const LIFT=0
 
-func _process(delta):
+@export var thrust_force: float = 200.0
+@export var gravity: float = 80.0
+@export var lift_multiplier: float = 10.5
+@export var drag_coefficient: float = 0.002
+@export var turn_speed: float = 3 
+@export var direction: Vector2 = Vector2(1,0)
+
+func _physics_process(delta: float) -> void:
 	#ROTATION
 	if Input.is_action_pressed("down"):
-		direction=direction.rotated(ANGLE)
-		rotate(ANGLE)
+		direction=direction.rotated(turn_speed*delta)
+		rotate(turn_speed*delta)
 	if Input.is_action_pressed("up"):
-		direction=direction.rotated(-ANGLE)
-		rotate(-ANGLE)
-	#APPLYING DRAG TO SPEED
-	var drag_force=DRAG*(pow(speed,2)/2)
-	speed =speed-drag_force
-	#PRE VELOCITY
-	velocity=direction*speed
+		direction=direction.rotated(-turn_speed*delta)
+		rotate(-turn_speed*delta)
+	#THRUST
+	if Input.is_action_pressed("thrust"):
+		velocity += direction * thrust_force*delta
+	#LIFT NEEDS TWEAKING
+	velocity+=direction.rotated(-1.5708)*lift_multiplier*delta
 	#GRAVITY
-	print(delta)
-	velocity+=GRAVITY*delta
-	print(velocity)
+	print(velocity.y)
+	if velocity.y<=200:
+		velocity.y += gravity * delta
+	#DRAG
+	velocity.x -= sign(velocity.x)*drag_coefficient*(pow(velocity.x,2)/2)*delta
+	velocity.y -= sign(velocity.y)*drag_coefficient*(pow(velocity.y,2)/2)*delta
 	move_and_slide()
